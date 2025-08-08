e688788d6d1e691df406db023b0587de13ebfbd5
qiangxu <toxuqiang@gmail.com>
2025-08-08 19:50:59 +0800

    feature: make through attest on base sepolia

--- src/attest-test.js
+++ src/attest-test.js
@@ -3,48 +3,45 @@ const { eas, SchemaEncoder } = require("./eas");
 const { signer, provider } = require("./client");
 
 
-const schemaUID = "0x213e6fb6cdca22ffb14f199dbe27101fbd23dcb1c3f6e01451dc1035774bd779"
+const schemaUID = "0xcb086ec854a03129a886407008b777592634fd93ae2a46565e0ea7d3e9a6cf23"
 
 async function main() {
-	const encoder = new SchemaEncoder("string order_id, bytes32 order_hash, uint64 timestamp");
+    const encoder = new SchemaEncoder(
+          "string BAR_NO,string PROJT_TYPE,string COUNTRY,uint256 BAR_VOL,uint256 BAR_AMT,string BUYER,string SELLER"
+    );
 
-	const order = {
-		order_id: "ORDER123",
-		user: "user001",
-		amount: "88.00",
-	};
+    const encoded = encoder.encodeData([ 
+        { name: 'BAR_NO', value: 'TX202508080001', type: 'string' },
+        { name: 'PROJT_TYPE', value: 'DigitalTrade', type: 'string' },
+        { name: 'COUNTRY', value: 'SG', type: 'string' },
+        { name: 'BAR_VOL', value: '1001234', type: 'uint256' },
+        { name: 'BAR_AMT', value: '888884321', type: 'uint256' },
+        { name: 'BUYER', value: 'Alibaba', type: 'string' },
+        { name: 'SELLER', value: 'Shopee', type: 'string' }
+    ]);
 
-	const orderHash = "0x" + crypto.createHash("sha256").update(JSON.stringify(order)).digest("hex");
+
+    // 发起交易
+    //
+    const tx = await eas.attest({
+        schema: schemaUID,
+        data: {
+            recipient: "0x0000000000000000000000000000000000000000",
+            expirationTime: 0,
+            revocable: true,
+            data: encoded,
+        },
+    });
 
 
-	const encoded = encoder.encodeData([
-		{ name: "order_id", value: order.order_id, type: "string" },
-		{ name: "order_hash", value: orderHash, type: "bytes32" },
-		{ name: "timestamp", value: Math.floor(Date.now() / 1000), type: "uint64" },
-	]);
+    console.log("⏳ Transaction submitted!");
 
-	// 发起交易
-	const tx = await eas.attest({
-		schema: schemaUID,
-		data: {
-			recipient: "0x0000000000000000000000000000000000000000",
-			expirationTime: 0,
-			revocable: true,
-			data: encoded,
-		},
-	});
-
-	// 不调用 tx.wait()，而是自己手动等交易完成（不触发 SDK 的事件解析）
-	console.log("⏳ Transaction submitted!");
-
-	const rawTx = await tx.wait();
-	console.log(rawTx)
-
+    const uid = await tx.wait();
+    console.log("✅ Attestation UID:", uid);  // 链上 attestation 唯一标识符
 }
 
 
-
 main().catch((err) => {
-	console.error("❌ Error occurred:", err);
+    console.error("❌ Error occurred:", err);
 });
 
--- src/client.js
+++ src/client.js
@@ -3,6 +3,7 @@ console.log("🛠️ Using RPC URL:", process.env.BASE_RPC_URL);
 const { JsonRpcProvider, Wallet } = require("ethers");
 
 const provider = new JsonRpcProvider(process.env.BASE_RPC_URL);
+provider.getNetwork().then(net => console.log("Connected to:", net.name, "Chain ID:", net.chainId));
 const signer = new Wallet(process.env.PRIVATE_KEY, provider);
 
 module.exports = { provider, signer };
